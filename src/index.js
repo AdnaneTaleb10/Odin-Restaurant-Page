414a484fe945d9d1ff133d5d9d92607ed0ed2717
Adnane <adnane102464@gmail.com>
2024-12-06 08:01:53 +0100

    Add modules files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+import home from "./home";
+import menu from "./menu";
+import about from "./about";
+
+const content = document.querySelector("#content");
+
+const loadHome = document.querySelector('#home');
+const loadMenu = document.querySelector('#menu');
+const loadAbout = document.querySelector('#about');
+
+loadHome.addEventListener('click', () => {
+    home();
+})
+
+loadMenu.addEventListener('click', () => {
+    menu();
+})
+
+loadAbout.addEventListener('click', () => {
+    about();
+})
